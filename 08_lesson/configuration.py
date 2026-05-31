60751d1210241b3189ea380346b0e9e68dcd2060
bengtssonulla08-oss <bengtssonulla08@gmail.com>
2026-05-31 13:19:42 +0300

    lesson8

--- 08_lesson/configuration.py
+++ 08_lesson/configuration.py
@@ -5,7 +5,3 @@ load_dotenv()
 
 TOKEN = os.getenv("YOUGILE_TOKEN")
 BASE_URL = os.getenv("YOUGILE_URL", "https://yougile.com")
-
-if not TOKEN:
-    raise ValueError(
-        "Ошибка: Токен YOUGILE_TOKEN не найден в переменных окружения!")
